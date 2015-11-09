b0cfdb5462a2e515562ca5e5b82d06e570885e9c
Loic CHOLLIER <loic@chollier.com>
2015-11-08 19:00:21 -0800

    Initial example commit

--- src/constants/actionTypes.js
+++
@@ -1,8 +0,0 @@
-import keyMirror from "key-mirror";
-
-export default keyMirror({
-  SELECT_CHANNEL: null,
-  LOADING_PLAYLIST_FROM_FILE: null,
-  PLAYLIST_LOADED_FROM_FILE: null,
-  PLAYLIST_LOAD_ERROR: null
-});
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import channels from "./channels";
-import ui from "./ui";
-
-export default combineReducers({
-  channels,
-  ui
-})
