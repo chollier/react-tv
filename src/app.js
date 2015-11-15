59ba809df9c44ddeff8426dba7349f12ed4f5a89
Loic CHOLLIER <loic@chollier.com>
2015-11-15 11:52:59 -0800

    Adds react-router

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import { render } from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { Router, Route } from "react-router";
+import createBrowserHistory from "history/lib/createBrowserHistory";
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={createBrowserHistory()}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider> ,
   document.getElementById("root")
 );
